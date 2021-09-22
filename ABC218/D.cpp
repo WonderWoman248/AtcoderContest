de8614970f634a22169b66d3c1efdcbd0e3e8145
prashansa15 <prashansa15111784@gmail.com>
2021-09-22 20:25:44 +0530

    D soln. added

--- ABC218/D.cpp
+++ ABC218/D.cpp
@@ -7,19 +7,48 @@ using namespace std;
 #define For(i, a, n) for (ll i = a; i < n;i++)
 #define Dfor(i, a, n) for (ll i = n-1; i >= a;i--)
 
-void solve(ll n, vrr points){
+// Checking if a point exit in the array.
+bool check(ll x, ll y,vector<pair<ll,ll>>points){
+    pair<ll, ll> find = make_pair(x, y);
+
+    // STL binary search function.
+    if(binary_search(points.begin(),points.end(),find)){
+        return true;
+    }
+
+    return false;
+}
+
+void solve(ll n, vector<pair<ll,ll>>points){
+    ll cnt = 0;
+    sort(points.begin(), points.end());
+    For(i,0,n-1){
+        For(j,i+1,n){
+            
+            // Checking for bottom left point and top right points exits.
+            if(points[i].first<points[j].first and points[i].second < points[j].second){
+
+                // Checking for rest two points.
+                if(check(points[i].first,points[j].second,points) and check(points[j].first,points[i].second,points)){
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    cout << cnt;
 }
 
 int main(){
     ll n;
     cin >> n;
 
-    vrr points(n, vr(2));
+    vector<pair<ll, ll>> points(n);
 
     For(i,0,n){
-        For(j,0,2){
-            cin >> points[i][j];
-        }
+        ll x, y;
+        cin >> x >> y;
+        points[i] = {x, y};
     }
     solve(n,points);
 }
